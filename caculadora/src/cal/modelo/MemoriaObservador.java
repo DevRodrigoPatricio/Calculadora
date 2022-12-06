d6387e108ed0896ead0aebb6c0e3b42d2def663c
rodrigo.pereira <rodrigo.pereira@group-note02193.group.local>
2022-12-06 20:17:27 -0300

    Calculadora

---
+++ caculadora/src/cal/modelo/MemoriaObservador.java
@@ -0,0 +1,7 @@
+package cal.modelo;
+
+@FunctionalInterface
+public interface MemoriaObservador {
+
+	void valorAlterado(String novoValor);
+}
---
+++ caculadora/src/cal/visao/Botao.java
@@ -0,0 +1,20 @@
+package cal.visao;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao  extends JButton	{
+
+	public Botao(String  texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		setFont(new Font("courier", Font.PLAIN, 25));
+	}
+}
